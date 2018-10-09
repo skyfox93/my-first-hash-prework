0e0fa9cfbc3f5c930774ccec4293563fe7b6cb2d
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-09 16:05:57 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,7 +6,7 @@ end
 
 def shipping_manifest
   the_manifest={}
-  the_manifest["Whale Bones"]=5
+  the_manifest["whale bone corsets"]=5
   the_manifest["porcelain vases"]=2
   the_manifest["oil paintings"]=3
   # set a variable called `the_manifest`, equal to a hash
